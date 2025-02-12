d22bc5d024c99501be4b685932eca401e68b2e37
kushrike <kushrike@gmail.com>
2025-02-13 01:35:57 +0530

    status-page: vercel deployment

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,12 +5,25 @@ function Login() {
   return (
     <Container maxWidth="sm" sx={{ mt: 8, mb: 4 }}>
       <Paper elevation={3} sx={{ p: 4 }}>
-        <SignIn
-          signUpUrl={undefined}
+        <SignIn 
+          signUpUrl="/sign-up"
           routing="path"
           path="/login"
           redirectUrl="/dashboard"
           afterSignInUrl="/dashboard"
+          appearance={{
+            layout: {
+              socialButtonsPlacement: 'bottom',
+              socialButtonsVariant: 'iconButton',
+              termsPageUrl: 'terms',
+              privacyPageUrl: 'privacy'
+            },
+            elements: {
+              rootBox: {
+                width: '100%'
+              }
+            }
+          }}
         />
       </Paper>
     </Container>
@@ -18,3 +31,4 @@ function Login() {
 }
 
 export default Login;
+
